0d778f317ccd13dae313220785bf7508b13e5937
Mamata24 <nmamta240@gmail.com>
2020-09-17 20:13:06 +0530

    Updated Navbar Functionalities in Maintenance Page

--- src/Components/Maintenance.jsx
+++ src/Components/Maintenance.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import Navbar from './Navbar'
 function FormItem(props) {
     return (
         <div
@@ -13,79 +13,83 @@ function FormItem(props) {
 function Maintenance() {
     function handleSubmit(e) {
         e.preventDefault();
-        window.cy.visit('https://ft4l0l2l.revvsales.com/documents/DOC-000011');
+        // window.cy.visit('https://ft4l0l2l.revvsales.com/documents/DOC-000011');
     }
 
     return (
-        <div
-            style={{
-                display: 'flex',
-                flexDirection: 'column',
-                alignItems: 'center',
-                justifyContent: 'center',
-                margin: '20px 0 40px 0',
-            }}
-        >
-            <h2 style={{ margin: '40px 0' }}>Maintenance Request Form</h2>
-            <form onSubmit={handleSubmit}>
-                <FormItem>
-                    <label
-                        label="House Number"
-                        htmlFor="house_no"
-                        style={{ fontWeight: '600', fontFamily: 'sans-serif' }}
-                    >
-                        House Number
+        <React.Fragment>
+            <Navbar />
+            <div
+                style={{
+                    display: 'flex',
+                    flexDirection: 'column',
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                    margin: '20px 0 40px 0',
+                }}
+            >
+                <h2 style={{ margin: '40px 0' }}>Maintenance Request Form</h2>
+                <form onSubmit={handleSubmit}>
+                    <FormItem>
+                        <label
+                            label="House Number"
+                            htmlFor="house_no"
+                            style={{ fontWeight: '600', fontFamily: 'sans-serif' }}
+                        >
+                            House Number
                     </label>
-                    <input
-                        style={{ margin: '10px 0' }}
-                        id="house_no"
-                        type="text"
-                        placeholder="Enter house number"
-                    />
-                </FormItem>
-                <FormItem>
-                    <label
-                        style={{ fontWeight: '600', fontFamily: 'sans-serif' }}
-                    >
-                        Contact Name
+                        <input
+                            style={{ margin: '10px 0' }}
+                            id="house_no"
+                            type="text"
+                            placeholder="Enter house number"
+                        />
+                    </FormItem>
+                    <FormItem>
+                        <label
+                            style={{ fontWeight: '600', fontFamily: 'sans-serif' }}
+                        >
+                            Contact Name
                     </label>
-                    <input style={{ margin: '10px 0' }} />
-                </FormItem>
-                <FormItem>
-                    <label
-                        style={{ fontWeight: '600', fontFamily: 'sans-serif' }}
-                    >
-                        Contact Number
+                        <input style={{ margin: '10px 0' }} />
+                    </FormItem>
+                    <FormItem>
+                        <label
+                            style={{ fontWeight: '600', fontFamily: 'sans-serif' }}
+                        >
+                            Contact Number
                     </label>
-                    <input style={{ margin: '10px 0' }} />
-                </FormItem>
-                <FormItem>
-                    <label
-                        style={{ fontWeight: '600', fontFamily: 'sans-serif' }}
-                    >
-                        Problem Category
+                        <input style={{ margin: '10px 0' }} />
+                    </FormItem>
+                    <FormItem>
+                        <label
+                            style={{ fontWeight: '600', fontFamily: 'sans-serif' }}
+                        >
+                            Problem Category
                     </label>
-                    <input style={{ margin: '10px 0' }} />
-                </FormItem>
-                <FormItem>
-                    <label
-                        style={{ fontWeight: '600', fontFamily: 'sans-serif' }}
-                    >
-                        Problem Description
+                        <input style={{ margin: '10px 0' }} />
+                    </FormItem>
+                    <FormItem>
+                        <label
+                            style={{ fontWeight: '600', fontFamily: 'sans-serif' }}
+                        >
+                            Problem Description
                     </label>
-                    <textarea style={{ margin: '10px 0' }} />
-                </FormItem>
-                <FormItem>
-                    <label
-                        style={{ fontWeight: '600', fontFamily: 'sans-serif' }}
-                    >
-                        Preffered Schedule
+                        <textarea style={{ margin: '10px 0' }} />
+                    </FormItem>
+                    <FormItem>
+                        <label
+                            style={{ fontWeight: '600', fontFamily: 'sans-serif' }}
+                        >
+                            Preffered Schedule
                     </label>
-                    <input style={{ margin: '10px 0' }} />
-                </FormItem>
-                <input type="submit" value="Submit" />
-            </form>
-        </div>
+                        <input style={{ margin: '10px 0' }} />
+                    </FormItem>
+                    <input type="submit" value="Submit" />
+                </form>
+            </div>
+        </React.Fragment>
+
     );
 }
 
